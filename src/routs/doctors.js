353dfb02f239c30e23db0626d09106bd955e5066
magweb <asukarikawa@gmail.com>
2022-06-05 13:22:50 +0600

    1

--- src/routs/doctors.js
+++ src/routs/doctors.js
@@ -45,12 +45,20 @@ router.get('/', (req, res) => {
 
 
 router.get('/doctorProfile/:username', (req, res) => {
+  var target = req.params.x
+  var curentUser = req.session.user.username
   var temp_doctor = req.params.username;
   mongoClient.connect(async function(error, mongo) {
       let db = mongo.db('tempbase');
       let doccoll = db.collection('doctors');
+      let coll = db.collection('users');
       let render_doctor = await doccoll.findOne({'username': temp_doctor});
-      res.render('doctorProfile', {render_doctor})
+      userData = await coll.findOne({'username' : target})
+        if(curentUser != undefined) {
+            res.render('doctorProfile', {render_doctor, userData, curentUser})
+        } else {
+            res.render('doctorProfile', {render_doctor, userData})
+        }
   });
 })
 
